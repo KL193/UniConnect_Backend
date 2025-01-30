bcf9f5a3ec418c3e8a1106834266559708fd5c24
KL193 <lakshankasthuriarachchi@gmail.com>
2025-01-30 15:34:55 +0530

    Initialize project structure

---
+++ src/main/java/com/USJ/UniConnect_Backend/UniConnectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.USJ.UniConnect_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniConnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniConnectBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/USJ/UniConnect_Backend/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.USJ.UniConnect_Backend.controller;
+
+public class UserController {
+
+}

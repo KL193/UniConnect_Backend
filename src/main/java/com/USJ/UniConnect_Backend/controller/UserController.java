7d5f67c9d7ce01197a017d1154495edfcdcf7691
KL193 <lakshankasthuriarachchi@gmail.com>
2025-02-03 22:42:49 +0530

    Add User module: Controller, DAO, Entity, and GET endpoint

--- src/main/java/com/USJ/UniConnect_Backend/controller/UserController.java
+++ src/main/java/com/USJ/UniConnect_Backend/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.USJ.UniConnect_Backend.controller;
 
+import com.USJ.UniConnect_Backend.dto.UserDto;
+import com.USJ.UniConnect_Backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/usercontroller")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+
 
 }
--- src/main/java/com/USJ/UniConnect_Backend/service/UserService.java
+++ src/main/java/com/USJ/UniConnect_Backend/service/UserService.java
@@ -1,5 +1,31 @@
 package com.USJ.UniConnect_Backend.service;
 
+
+import com.USJ.UniConnect_Backend.dao.UserDao;
+import com.USJ.UniConnect_Backend.dto.UserDto;
+import com.USJ.UniConnect_Backend.entities.UserEntity;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<UserDto> getAllUsers() {
+
+        List<UserEntity> userList = userDao.findAll();
+        return modelMapper.map(userList,new TypeToken<List<UserDto>>(){}.getType());
+    }
 
 }

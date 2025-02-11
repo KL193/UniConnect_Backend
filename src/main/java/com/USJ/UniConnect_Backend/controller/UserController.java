235bc28474c629c20845a3f51e53e1f6b7eeb5ee
KL193 <lakshankasthuriarachchi@gmail.com>
2025-02-11 23:12:29 +0530

    Add util package

--- src/main/java/com/USJ/UniConnect_Backend/controller/UserController.java
+++ src/main/java/com/USJ/UniConnect_Backend/controller/UserController.java
@@ -3,7 +3,10 @@ package com.USJ.UniConnect_Backend.controller;
 import com.USJ.UniConnect_Backend.dto.UserDto;
 import com.USJ.UniConnect_Backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +16,22 @@ import java.util.List;
 @RequestMapping("api/v1/usercontroller")
 public class UserController {
 
-    @Autowired
+
     private UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+
+        this.userService  = userService ;
+    }
+
     @GetMapping("/getusers")
-    public List<UserDto> getAllUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<UserDto>>  getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
 
-
 }
+
+
+
--- src/main/java/com/USJ/UniConnect_Backend/service/UserService.java
+++ src/main/java/com/USJ/UniConnect_Backend/service/UserService.java
@@ -4,6 +4,7 @@ package com.USJ.UniConnect_Backend.service;
 import com.USJ.UniConnect_Backend.dao.UserDao;
 import com.USJ.UniConnect_Backend.dto.UserDto;
 import com.USJ.UniConnect_Backend.entities.UserEntity;
+import com.USJ.UniConnect_Backend.util.EntityDtoConversion;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -21,11 +22,13 @@ public class UserService {
 
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private EntityDtoConversion entityDtoConversion;
 
     public List<UserDto> getAllUsers() {
 
         List<UserEntity> userList = userDao.findAll();
-        return modelMapper.map(userList,new TypeToken<List<UserDto>>(){}.getType());
+        return entityDtoConversion.toUserDtoList(userList);
     }
 
 }
